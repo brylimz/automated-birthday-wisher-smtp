866c7ef1c834ed534ebe7378de8845a9cebdeecc
Bryan`Y. Limz <superuser@brylimz.com>
2021-12-28 21:46:55 +0700

    office

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 import smtplib
-my_email = "brylimz@gmail.com"
+my_email = "jackjaggeriv@gmail.com"
+password = "easefareaseasefareaseasefareaseasefareas"
 connection = smtplib.SMTP("smtp.gmail.com")
 connection.starttls()
+connection.login(user=my_email, password=password)
+connection.sendmail(from_addr=my_email, to_addrs="superuser@brylimz.com", msg="Hello")
+connection.close ()
