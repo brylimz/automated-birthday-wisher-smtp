a18302ed37318820abe89c8c5373cf779287433b
brylimz <superuser@brylimz.com>
2021-12-29 11:09:18 +0700

    new pass

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import smtplib
 my_email = "jackjaggeriv@gmail.com"
-password = "easefareaseasefareaseasefareaseasefareas"
+password = "hionfetkseoeoilt"
 connection = smtplib.SMTP("smtp.gmail.com")
 connection.starttls()
 connection.login(user=my_email, password=password)
 connection.sendmail(from_addr=my_email, to_addrs="superuser@brylimz.com", msg="Hello")
-connection.close ()
+connection.close()
+
